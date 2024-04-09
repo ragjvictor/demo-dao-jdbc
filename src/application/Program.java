74fb8fd47246f86dfcad1e5b30f743095a4dd9b8
Jose Victor <ragjvictor@gmail.com>
2024-04-08 23:01:18 -0400

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,6 +35,12 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("\nInserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Zezo dos Jacaré");
+		sellerDao.update(seller);
+		System.out.println("\nUpdate completed");
 	}
 
 }
