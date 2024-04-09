45b6680676d1612378bed6ba2538b6605a3e1417
Jose Victor <ragjvictor@gmail.com>
2024-04-08 23:26:19 -0400

    Department findById implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,9 +1,18 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
 public class Program2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("=== TEST 1: department findById ===");
+		Department department = departmentDao.findById(3);
+		System.out.println(department);
 
 	}
 
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,8 +1,13 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
@@ -34,8 +39,27 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 
 	@Override
 	public Department findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement("SELECT name FROM department WHERE id = ?");
+			
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			if (!rs.next()) return null;
+			
+			Department dep = new Department();
+			dep.setId(id);
+			dep.setName(rs.getString("name"));
+			return dep;
+			
+		} catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
